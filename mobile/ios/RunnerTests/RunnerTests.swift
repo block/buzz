58a0ebeffa768ddfe43208b62a51d343c7d21dcd
kenny lopez <klopez4212@gmail.com>
2026-07-24 11:35:00 -0700

    Use safe area for Dynamic Island detection

--- mobile/ios/RunnerTests/RunnerTests.swift
+++ mobile/ios/RunnerTests/RunnerTests.swift
@@ -6,32 +6,24 @@ import XCTest
 
 class RunnerTests: XCTestCase {
 
-  func testDynamicIslandQrScannerRecognizesSupportedIPhones() {
-    for modelIdentifier in [
-      "iPhone15,2", "iPhone15,3",
-      "iPhone15,4", "iPhone15,5", "iPhone16,1", "iPhone16,2",
-      "iPhone17,1", "iPhone17,2", "iPhone17,3", "iPhone17,4",
-      "iPhone18,1", "iPhone18,2", "iPhone18,3", "iPhone18,4",
-    ] {
+  func testDynamicIslandQrScannerRecognizesTallSafeAreas() {
+    for safeAreaTopInset in [51, 59, 62] {
       XCTAssertTrue(
         AppDelegate.usesDynamicIslandQrScannerPortal(
-          modelIdentifier: modelIdentifier
+          safeAreaTopInset: CGFloat(safeAreaTopInset)
         ),
-        modelIdentifier
+        "\(safeAreaTopInset)"
       )
     }
   }
 
-  func testDynamicIslandQrScannerRejectsUnsupportedDevices() {
-    for modelIdentifier in [
-      "iPhone14,7", "iPhone14,8", "iPhone17,5", "iPhone18,5",
-      "iPad16,3", "arm64",
-    ] {
+  func testDynamicIslandQrScannerRejectsStandardSafeAreas() {
+    for safeAreaTopInset in [0, 44, 47, 50] {
       XCTAssertFalse(
         AppDelegate.usesDynamicIslandQrScannerPortal(
-          modelIdentifier: modelIdentifier
+          safeAreaTopInset: CGFloat(safeAreaTopInset)
         ),
-        modelIdentifier
+        "\(safeAreaTopInset)"
       )
     }
   }
